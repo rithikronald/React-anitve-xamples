010c44b29cc2c91883ffb16529867015a7c02568
rithikronald <rithikronald29@gmail.com>
2024-11-25 10:45:27 +0530

    Removed

--- App.tsx
+++ App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import {View} from 'react-native';
 import './global.css';
-import {Provider} from 'react-redux';
-import {ReduxTodo} from './component/ReduxTodo';
-import {store2} from './state/store2';
 
 function App(): React.JSX.Element {
-  return (
-    <View className="flex flex-1 justify-center items-center p-4">
-      <Provider store={store2}>
-        <ReduxTodo />
-      </Provider>
-    </View>
-  );
+  return <View></View>;
 }
 
 export default App;
--- state/store2.ts
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {todoReducer} from './todo/todoslice';
-
-export const store2 = configureStore({
-  reducer: {
-    todo: todoReducer,
-  },
-});
-f
