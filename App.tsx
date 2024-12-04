9be0d44ca193beb5c24798af1e64bf30cfbf6b47
rithikronald <rithikronald29@gmail.com>
2024-12-04 13:46:27 +0530

    Removed Code

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import './global.css';
-import {ReduxCounter} from './src/component/ReduxCounter';
-import {Provider} from 'react-redux';
-import {store} from './src/state/store';
 
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <View className="flex justify-center items-center p-4 mt-10">
-        <ReduxCounter />
-      </View>
-    </Provider>
+    <View className="flex flex-1 justify-center items-center">
+      <Text>Hello World</Text>
+    </View>
   );
 }
 
